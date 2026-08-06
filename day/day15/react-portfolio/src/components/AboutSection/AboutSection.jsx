281fe82dd0ded9bc6a162cb45e2ef198cd436998
iksha1947-png <iksha1947@gmail.com>
2026-08-06 15:01:17 +0530

    aboutsection.jsx

--- day/day15/react-portfolio/src/components/AboutSection/AboutSection.jsx
+++ day/day15/react-portfolio/src/components/AboutSection/AboutSection.jsx
@@ -5,7 +5,7 @@ function AboutSection() {
     <section id='about' className='about-section'>
       <div className='about-wrapper container'>
         <div className='about-image'>
-          <img src='/Photo.jpeg' alt='Iksha' />
+          <img src='/Photo.jpeg.jpeg' alt='Iksha' />
         </div>
         <div className='about-copy'>
           <h1>About Me</h1>
